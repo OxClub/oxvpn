d7a14aa8496af7073afeb426f7c9129be2b27a1b
Oxclub <Oxclub@users.noreply.github.com>
2026-09-20 14:07:34 +0530

    All Android 16 VPN fixes: FGS type, AAB native lib, permission handler

--- android/app/src/main/kotlin/com/oxclub/oxvpn/MainActivity.kt
+++ android/app/src/main/kotlin/com/oxclub/oxvpn/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.oxclub.oxvpn
 
+import android.content.Intent
+import id.laskarmedia.openvpn_flutter.OpenVPNFlutterPlugin
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(engine: FlutterEngine) {
-        super.configureFlutterEngine(engine)
-        // Register oxvpn/control MethodChannel and oxvpn/state EventChannel here.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        OpenVPNFlutterPlugin.connectWhileGranted(requestCode == 24 && resultCode == RESULT_OK)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
